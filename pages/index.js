aba71ac5e6ebd0c33302413ae7cf70bc0f1ffbff
yvan-nguekam <72554193+yvan-nguekam@users.noreply.github.com>
2022-12-09 17:13:45 +0100

    dropdown en cours

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
-import { Card } from "../components/Card";
 import { NavigationCard } from "../components/NavigationCard";
+import { PostCard } from "../components/PostCard";
 import { PostFormCard } from "../components/PostFormCard";
 
 export default function Home() {
   return (
     <div className='flex mt-4 max-w-4xl mx-auto gap-6'>
-      <div className="w-1/3">
+      <div className="w-3/12">
         <NavigationCard/>
       </div>
-      <div className="grow">
+      <div className="w-9/12">
         <PostFormCard/>
-        <Card>First post test</Card>
+        <PostCard/>
       </div>
     </div>
   )
