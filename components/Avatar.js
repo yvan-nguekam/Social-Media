9fbc463031ce49dadb509ce4f6c27f186ea2e542
yvan-nguekam <72554193+yvan-nguekam@users.noreply.github.com>
2022-12-09 15:14:57 +0100

    post from end

---
+++ components/Avatar.js
@@ -0,0 +1,7 @@
+export function Avatar() {
+    return (
+        <div className="w-12 rounded-full overflow-hidden">
+            <img src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
+        </div>
+    )
+}
